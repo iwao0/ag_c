1a6751672ce6c3681fd1ed8fe4dddb6ae354ddca
iwao <106776059+iwao0@users.noreply.github.com>
2026-03-24 15:29:22 +0900

    parser: factor toplevel member array-suffix parsing

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -89,6 +89,7 @@ static token_t *skip_decl_prefix_lookahead(token_t *t);
 static token_kind_t parse_atomic_type_specifier(void);
 static int parse_array_size_constexpr_toplevel(void);
 static int parse_array_size_optional_constexpr_toplevel(int *out_has_size);
+static int parse_toplevel_member_array_suffixes(int *out_is_flex_array);
 typedef struct {
   int arr_total;
   int is_array;
@@ -614,6 +615,22 @@ static int parse_toplevel_array_suffixes_constexpr_required(int base_mul) {
   return arr_total;
 }
 
+static int parse_toplevel_member_array_suffixes(int *out_is_flex_array) {
+  int arr_total = 1;
+  int is_flex_array = 0;
+  while (tk_consume('[')) {
+    if (curtok()->kind == TK_RBRACKET) {
+      is_flex_array = 1;
+      arr_total = 0;
+    } else {
+      arr_total *= parse_array_size_constexpr_toplevel();
+    }
+    tk_expect(']');
+  }
+  if (out_is_flex_array) *out_is_flex_array = is_flex_array;
+  return arr_total;
+}
+
 static void parse_toplevel_declarator_list(void) {
   int declarator_count = 0;
   for (;;) {
@@ -918,19 +935,11 @@ static int parse_struct_or_union_members_layout_toplevel(token_kind_t tag_kind, 
       bf_storage_offset = -1;
       bf_bits_used = 0;
 
-      int arr_size = 1;
       int is_flex_array = 0;
-      while (tk_consume('[')) {
-        if (!has_member_name) psx_diag_missing(curtok(), diag_text_for(DIAG_TEXT_MEMBER_NAME));
-        if (curtok()->kind == TK_RBRACKET) {
-          // フレキシブル配列メンバー: int data[];
-          is_flex_array = 1;
-          arr_size = 0;
-        } else {
-          arr_size *= parse_array_size_constexpr_toplevel();
-        }
-        tk_expect(']');
+      if (curtok()->kind == TK_LBRACKET && !has_member_name) {
+        psx_diag_missing(curtok(), diag_text_for(DIAG_TEXT_MEMBER_NAME));
       }
+      int arr_size = parse_toplevel_member_array_suffixes(&is_flex_array);
       if (paren_array_mul > 1) arr_size *= paren_array_mul;
       int member_elem_size = is_ptr ? 8 : elem_size;
       int total_size = is_flex_array ? 0 : (member_elem_size * arr_size);
