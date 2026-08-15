1c22963e0f184947e2e7e5e827692d88b1243fd4
iwao <106776059+iwao0@users.noreply.github.com>
2026-08-15 12:31:24 +0900

    Cover phase-swapped missing-mask Gray cycle

--- test/test_language_analysis.c
+++ test/test_language_analysis.c
@@ -6855,6 +6855,10 @@ static int test_enum_three_argument_call_cursor(
       {11, 2}, {4, 3}, {11, 1}, {4, 0},
       {5, 0}, {10, 4}, {5, 5}, {10, 7}, {5, 6},
       {10, 2}, {5, 3}, {10, 1}, {5, 0},
+      {4, 1}, {11, 3}, {4, 2}, {11, 6}, {4, 7},
+      {11, 5}, {4, 4}, {11, 0}, {4, 1},
+      {5, 1}, {10, 3}, {5, 2}, {10, 6}, {5, 7},
+      {10, 5}, {5, 4}, {10, 0}, {5, 1},
       {4, 0}, {10, 0}};
   const char *callee_name = "ENUM_THREE_ARGUMENT_CALL";
   size_t callee_length = strlen(callee_name);
